12850227a3b8d3efb47da47a35c3dc7ccf524cfa
kyouko17 <motyadoto1@gmail.com>
2026-02-05 11:39:27 +0300

    full build

--- main.go
+++ main.go
@@ -2,24 +2,86 @@ package main
 
 import (
 	"crypto/rand"
+	"errors"
+	"flag"
 	"fmt"
 	"math/big"
-	"strings"
+	"os"
 )
 
-var Whitelist string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@$#%/?-~"
+const (
+	whitelist     string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@$#%/?-~"
+	lowerCase     string = "abcdefghijklmnopqrstuvwxyz"
+	upperCase     string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numbers       string = "1234567890"
+	uniqueSymbols string = "!@$#%/?-~"
+)
+
+var ErrNotAcceptableLength = errors.New("password length must be between 4 and 32")
 
 func main() {
-	fmt.Println(PasswordGenerator(8))
-}
-func PasswordGenerator(length int) string {
-	var sb strings.Builder
-	max := big.NewInt(int64(len(Whitelist)))
+	length := flag.Int("length", 8, "length of password (4-32)")
+	flag.Parse()
 
-	for range length {
-		n, _ := rand.Int(rand.Reader, max)
-		sb.WriteRune(rune(Whitelist[n.Int64()]))
+	password, err := PasswordGenerator(*length)
+	if err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
+	}
+	fmt.Println("generated password:", password)
+}
+
+func PasswordGenerator(length int) (string, error) {
+	//Error handle
+	if length < 4 || length > 32 {
+		return "", ErrNotAcceptableLength
 	}
 
-	return sb.String()
+	var res []byte
+
+	//Add base for pass
+	err := addRandomCharFromString(&res, lowerCase)
+	if err != nil {
+		return "", err
+	}
+	err = addRandomCharFromString(&res, upperCase)
+	if err != nil {
+		return "", err
+	}
+	err = addRandomCharFromString(&res, numbers)
+	if err != nil {
+		return "", err
+	}
+	err = addRandomCharFromString(&res, uniqueSymbols)
+	if err != nil {
+		return "", err
+	}
+
+	//Fill remaining
+	for i := 0; i < length-4; i++ {
+		err := addRandomCharFromString(&res, whitelist)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	//Shuffle (Fisher-Yates)
+	for i := len(res) - 1; i > 0; i-- {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(i+1)))
+		if err != nil {
+			return "", err
+		}
+		res[i], res[n.Int64()] = res[n.Int64()], res[i]
+	}
+
+	return string(res), nil
+}
+
+func addRandomCharFromString(res *[]byte, source string) error {
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(len(source))))
+	if err != nil {
+		return err
+	}
+	*res = append(*res, source[n.Int64()])
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -4,11 +4,27 @@ import "testing"
 
 func TestPasswordGenerator(t *testing.T) {
 	t.Run("length of password", func(t *testing.T) {
-		got := len(PasswordGenerator(16))
+		password, err := PasswordGenerator(16)
+		got := len(password)
 		want := 16
 
+		if err != nil {
+			t.Errorf("unexpected error: %g", err)
+		}
 		if got != want {
 			t.Errorf("expected length: %d, gotten legth: %d", got, want)
+		}
+	})
+	t.Run("length out of range", func(t *testing.T) {
+		password, err := PasswordGenerator(3)
+		got := password
+		want := ""
+
+		if err != ErrNotAcceptableLength {
+			t.Errorf("")
+		}
+		if got != want {
+			t.Errorf("got: %s, want: %s", got, want)
 		}
 	})
 }
